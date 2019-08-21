74592d9750726257a55be15cc0bc85b72c6f5d7c
GheorghePavel <gheorghe.pavel1@gmail.com>
2019-08-21 22:43:43 +0300

    first commit

---
+++ src/components/users/Users.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+class Users extends Component {
+  state = {
+    users: [
+      {
+        id: "1",
+        login: "mojombo",
+        avatar_url: "",
+        html_url: ""
+      },
+      {
+        id: "1",
+        login: "mojombo",
+        avatar_url: "",
+        html_url: ""
+      },
+      {
+        id: "1",
+        login: "mojombo",
+        avatar_url: "",
+        html_url: ""
+      }
+    ]
+  };
+  render() {
+    return <div />;
+  }
+}
+
+export default Users;
